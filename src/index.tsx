449a02a71a298310fbb2b1d9aa58ff7c368500f3
kelvi <kelvi_ugo@hotmail.com>
2022-06-10 14:05:15 -0300

    feat: export components by index

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+export { Button, ButtonProps } from "./components/Button";
+export { Input, InputProps } from "./components/Input";
+
+export {
+  Input as InputChakra,
+  InputProps as InputChakraProps,
+  Button as ButtonChakra,
+  ButtonProps as ButtonChakraProps,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  color,
+} from "./components/ChakraUI";
