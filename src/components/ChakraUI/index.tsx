284c4ad274cb3a92940271a8b27c62cee02ef070
kelvi <kelvi_ugo@hotmail.com>
2022-06-10 14:03:59 -0300

    feat: export components chakra-ui to test the library

---
+++ src/components/ChakraUI/index.tsx
@@ -0,0 +1,11 @@
+export { Input, InputProps } from "@chakra-ui/react";
+export { Button, ButtonProps } from "@chakra-ui/react";
+export {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  color,
+} from "@chakra-ui/react";
